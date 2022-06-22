2d42083534479939e92efdf4127697d353245e33
AndreiRogachevski <arogov89@gmail.com>
2022-06-22 21:18:09 +0300

    lesson32_createElement

--- lesson31_DOM/hw31.js
+++ lesson31_DOM/hw31.js
@@ -1,25 +1,23 @@
 //TASK1
 let p = document.body.querySelectorAll('p')
-const textP= 'text3';
+const textP = 'text3';
 for (let n = 0; n < p.length; n++) {
   //Удалите тег p с текстом text3
-  if(p[n].textContent.includes(textP)){
+  if (p[n].textContent.includes(textP)) {
     delete p[n]
   }
   //Сделайте текст в четных(по порядку следования) тегах p подчеркнутым
   if (n % 2) {
     p[n].style.textDecoration = '2px underline solid black';
   }
-  if(p[n].textContent==textP){
-    p[n].innerHTML=''
+  if (p[n].textContent == textP) {
+    p[n].innerHTML = ''
   }
-  // nm = p[n].className;
-  // p[n].classList.remove('nm')
+
 }
-// p.forEach((el) => {
-//   elN = el.className;
-//   el.classList.remove('elN')
-// })
+p.forEach((el) => {
+  el.className =''
+})
 console.log(p);
 //Добавьте всем элементам li CSS-класс item
 const li = document.body.querySelectorAll('li');
@@ -28,11 +26,21 @@ console.log(li);
 
 //TASK2
 const secondTask = document.body.querySelector('.task2')
-const firstDiv = document.body.children
-firstDiv
-console.log(firstDiv);
+// const firstDiv = secondTask.children
+let divs = secondTask.querySelectorAll('div');
+divs.forEach((el) => {
+  if (el.children.length === 0) {
+    el.style.background = 'blue';
+  } 
+  else if(el.children.length === 1){
+    el.style.background = 'yellow';
+  }else if(el.children.length === 2){
+    el.style.background = 'grey';
+  }
+
+  console.log(el.children.length);
+})
+console.log(div);
 
 //TASK3
-let div = document.body.querySelectorAll('div');
-console.log(div);
-div.forEach((el) => {el.style.color = `${el.textContent}`})
+let div1 = document.body.querySelectorAll('div').forEach((el) => { el.style.background = el.textContent })
