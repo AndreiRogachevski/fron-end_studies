062ad2953309a14f8e5429e9f95c7e2871b66e2b
AndreiRogachevski <Arogov89@gmail.com>
2022-12-14 14:33:32 +0300

    hw68

---
+++ lesson68/hw/src/App.js
@@ -0,0 +1,37 @@
+import './App.css';
+const data = {
+  labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+  datasets: [
+    {
+      label: '# of Votes',
+      data: [12, 19, 3, 5, 2, 3],
+      borderWidth: 1,
+    },
+  ],
+};
+function createTableRow(number) {
+  const rows = [];
+  for (let index = 1; index < number/2; index++) {
+    rows.push(index);
+  }
+  console.log(rows);
+}
+console.log(Math.max(data.datasets[0].data));
+function App() {
+  return (
+    <div className="App">
+      <div className="tableGrid" style={{}}>
+        {createTableRow(Math.max(...data.datasets[0].data))}
+      </div>
+      <div className="table">
+        {data.datasets[0].data.map((item, index) => (
+          <div key={index} className="tableItem" style={{ height: item * 10 }}>
+            <span>{data.labels[index]}</span>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default App;
